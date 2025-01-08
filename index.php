89aa5d03bf058e2ff9c8eb12d2b32ffaa963b89f
vysan12 <vios49899@gmail.com>
2025-01-08 08:35:36 +0700

    publish

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+// Create database connection using config file
+include_once("config.php");
+ 
+// Fetch all users data from database
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
+?>
+ 
+<html>
+<head>    
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+ 
+<body>
+    <div class="py-2">
+        <img src="./logo.png" alt="logo" class="mx-auto d-block">
+        <h1 class="text-center">Dashboard Manajemen Users</h1><br>
+    </div> 
+    <table border=1 class=1 class="table">
+
+ 
+    <table width='80%' border=1>
+ 
+    <tr>
+        <th>Name</th> <th>Mobile</th> <th>Email</th> <th>Update</th>
+    </tr>
+    <?php  
+    while($user_data = mysqli_fetch_array($result)) {         
+        echo "<tr>";
+        echo "<td>".$user_data['name']."</td>";
+        echo "<td>".$user_data['mobile']."</td>";
+        echo "<td>".$user_data['email']."</td>";    
+        echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
+    }
+    ?>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
